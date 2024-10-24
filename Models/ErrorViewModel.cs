d817824703bdc3464e5ea9704f31db788ec17ef5
Göksel Karadağ <goksel.karadag34@gmail.com>
2024-10-24 18:48:20 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjaxPaginition.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
